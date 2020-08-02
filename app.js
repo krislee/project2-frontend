c94c32a36a75a1077cb1cbcce688bcfa4d550e3f
Kristy Lee <leekristy01@gmail.com>
2020-08-01 22:42:28 -0400

    navigation bar set up with minimal CSS

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+
+
+/////// DISPLAY ALL ///////
+
+const getAll = async () => {
+    const response = await fetch()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////// NAVIGATION BAR //////
+let $burger = $('.burger')
+$burger.on('click', function(e) {
+    let $right = $('.right-burger');
+    $right.toggleClass('show');
+    $('.burger').toggleClass('xcross')
+})
+
+$(window).resize (function(e) {
+   if($(window).width()>600){
+       $('.right-burger').removeClass('show')
+       $('.burger').removeClass('xcross')
+   }
+})
