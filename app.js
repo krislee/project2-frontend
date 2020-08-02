4283487bc4e6b5ba4e2749007baec1e53627e800
Kristy Lee <leekristy01@gmail.com>
2020-08-02 11:52:28 -0400

    finished the show one blog

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const getAll = async () => {
         .attr({'id': blog._id, 'class': 'placeDiv'})
         .text(`${blog.destination}`)
         .on('click', showOneBlog)
-        .on('click', removeDisplayedBlog)
+        // .on('click', removeDisplayedBlog)
     $('#listAllBlogs').append($placediv)
     })
 }
@@ -24,22 +24,62 @@ const getAll = async () => {
 const showOneBlog = async () => {
     const response = await fetch(`${URL}/travel/${event.target.id}`)
     const data = await response.json()
-    
+
+    $('#listOneBlog').empty()
+
     const $oneBlog = $('<div>').attr({'id': data._id, 'class': 'oneBlog'})
     const $title = $('<h1>').attr('class', 'headingTitle').text(`${data.destination}`)
     const $imageCover = $('<img>').attr({'src': `${data.image}`, 'class': 'image-cover'})
     
-    const $contentDiv = $('<div>').html(`<p><span class="favoriteHeading">Favorite Heading</span>${data.content[0].favoriteMemory}</p>`)
+    const $contentParaDiv = $('<div>')
+    .attr('id', 'paraDiv')
+    .html(
+        `<p>
+        <span class="favoriteHeading">Favorite Memory: </span>
+        ${data.content[0].favoriteMemory} </br> </br> <span class="leastFavoriteHeading"> Least Favorite Memory: </span>
+        ${data.content[0].leastFavoriteMemory}
+        </p>
+        `
+    )
+    
+    // $landRestDiv is the flex div to make $landmarkDiv and $restaurantDiv in rows
+    const $landRestDiv = $('<div>').attr('class', 'landRestDiv')
 
-    $oneBlog.append($title).append($imageCover).append($contentDiv)
+    // LANDMARK DIV, HEADING, UL, LI
+    const $landmarkDiv = $('<div>').attr('id', 'landmarkDiv')
+    const $landmarkHeading = $('<h3>').text('Landmark').attr('class', 'landmarkHeading')
+    $landmarkDiv.append($landmarkHeading)
+    $listLandmarkContainer = $('<ul>').attr('id', 'listLandmarkContainer')
+    data.content[0].landmark.forEach(landmark => {
+        const $landmarkList = $('<li>').text(`${landmark}`)
+        $listLandmarkContainer.append($landmarkList)
+    })
+    $landmarkDiv.append($listLandmarkContainer)
+
+    // RESTAURANT DIV, HEADING, UL, LI
+    const $restaurantDiv = $('<div>').attr('id', 'restaurantDiv')
+    const $restaurantHeading = $('<h3>').text('Restaurant').attr('class', 'restaurantHeading')
+    $restaurantDiv.append($restaurantHeading)
+    $listRestaurantContainer = $('<ul>').attr('id', 'listRestaurantContainer')
+    data.content[0].restaurant.forEach(restaurant => {
+        const $restaurantList = $('<li>').text(`${restaurant}`)
+        $listRestaurantContainer.append($restaurantList)
+    })
+    $restaurantDiv.append($listRestaurantContainer)
+
+    // RATINGS
+    const $ratingDiv = $('<div>')
+    .attr('class', 'ratingDiv')
+    .html(`<span class="ratingHeading"> Rating: </span> ${data.content[0].rating}`)
+    
+    $landRestDiv.append($landmarkDiv).append($restaurantDiv)
+    $oneBlog.append($title).append($imageCover).append($contentParaDiv).append($landRestDiv).append($ratingDiv)
     $('#listOneBlog').append($oneBlog)
     console.log(data)
 
 }   
 
-// const removeDisplayedBlog = () => {
-//     if (event.target.id !== )
-// }
+
 getAll()
 
 
